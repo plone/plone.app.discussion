ee5aa9ae4d664be19b1bbbef248ff6979572d0c6
Timo Stollenwerk <timo@zmag.de>
2009-05-31 17:49:33 +0000

    Monkey patch plone.app.vocabularies.types.BAD_TYPES
    svn path=/plone.app.discussion/trunk/; revision=27245

--- plone/app/discussion/__init__.py
+++ plone/app/discussion/__init__.py
@@ -1,1 +1,11 @@
+# Monkey patch plone.app.vocabularies.types.BAD_TYPES and remove
+# 'Discussion Item' from this tuple, so that Comments can be found
+# in the search. This will become needless once plone.app.discussion
+# will become a part of Plone 4.
 
+import plone.app.vocabularies.types
+
+new_bad_types = list(plone.app.vocabularies.types.BAD_TYPES)
+if 'Discussion Item' in new_bad_types:
+    new_bad_types.remove('Discussion Item')
+plone.app.vocabularies.types.BAD_TYPES = new_bad_types
