83b631413aea78139f3484ef62018bda9f8e5cdc
Timo Stollenwerk <timo@zmag.de>
2009-10-12 18:46:03 +0000

    revert r30055, since p.a.d. will not become part of Plone 4.
    svn path=/plone.app.discussion/trunk/; revision=30462

--- plone/app/discussion/__init__.py
+++ plone/app/discussion/__init__.py
@@ -3,9 +3,9 @@
 # in the search. This will become needless once plone.app.discussion
 # will become a part of Plone 4.
 
-#import plone.app.vocabularies.types
-#
-#new_bad_types = list(plone.app.vocabularies.types.BAD_TYPES)
-#if 'Discussion Item' in new_bad_types:
-#    new_bad_types.remove('Discussion Item')
-#plone.app.vocabularies.types.BAD_TYPES = new_bad_types
+import plone.app.vocabularies.types
+
+new_bad_types = list(plone.app.vocabularies.types.BAD_TYPES)
+if 'Discussion Item' in new_bad_types:
+    new_bad_types.remove('Discussion Item')
+plone.app.vocabularies.types.BAD_TYPES = new_bad_types
