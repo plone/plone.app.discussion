7ef4f384e0139522b2e72337e96eba6e31071825
Timo Stollenwerk <timo@zmag.de>
2010-03-17 10:42:37 +0000

    add additional hint to the "globally enabled" setting. respect 80 characters limit.
    svn path=/plone.app.discussion/trunk/; revision=35079

--- plone/app/discussion/interfaces.py
+++ plone/app/discussion/interfaces.py
@@ -17,41 +17,54 @@ class IDiscussionSettings(Interface):
     # - Types control panel: Allow comments on content types
     # - Search control panel: Show comments in search results
 
-    globally_enabled = schema.Bool(title=_(u"label_globally_enabled",
-                                           default=u"Globally enable comments"),
-                                   description=_(u"help_globally_enabled",
-                                                 default=u"If selected, users are able to "
-                                                          "post comments on the site."),
-                                   required=False,
-                                   default=True)
+    globally_enabled = schema.Bool(
+                           title=_(u"label_globally_enabled",
+                                   default=u"Globally enable comments"),
+                           description=_(u"help_globally_enabled",
+                                         default=u"If selected, users are able "
+                                                  "to post comments on the site"
+                                                   ". Though, you have to "
+                                                   "enable comments for "
+                                                   "specific content types, "
+                                                   "folders or content objects"
+                                                   "before users will be able "
+                                                   "to post comments."),
+                           required=False,
+                           default=True)
 
-    anonymous_comments = schema.Bool(title=_(u"Enable anonymous comments"),
-                                     description=_(u"help_anonymous_comments",
-                                                   default=u"If selected, anonymous users are "
-                                                            "able to post comments without "
-                                                            "logging in. It is highly " 
-                                                            "recommended to use a captcha "
-                                                            "solution to prevent spam if "
-                                                            "this setting is enabled."),
-                                     required=False,
-                                     default=False)
+    anonymous_comments = schema.Bool(
+                             title=_(u"Enable anonymous comments"),
+                             description=_(u"help_anonymous_comments",
+                                           default=u"If selected, anonymous "
+                                                    "users are able to post "
+                                                    "comments without loggin "
+                                                    "in. It is highly " 
+                                                    "recommended to use a "
+                                                    "captcha solution to "
+                                                    "prevent spam if this "
+                                                    "setting is enabled."),
+                             required=False,
+                             default=False)
 
-    captcha = schema.Choice(title=_(u"Captcha"),
-                            description=_(u"help_captcha",
-                                          default=u"Use this setting to enable or disable " 
-                                                   "captcha validation for comments. If no "
-                                                   "captcha options are currently available, "
-                                                   "install plone.formwidget.captcha or " 
-                                                   "plone.formwidget.recaptcha."),
-                            required=True,
-                            default='disabled',
-                            vocabulary='plone.app.discussion.vocabularies.CaptchaVocabulary',)
+    captcha = schema.Choice(
+                  title=_(u"Captcha"),
+                  description=_(u"help_captcha",
+                                default=u"Use this setting to enable or "
+                                         "disable Captcha validation for "
+                                         "comments. Install "
+                                         "plone.formwidget.captcha or "
+                                         "plone.formwidget.recaptcha if there "
+                                         "are no options available."),
+                   required=True,
+                   default='disabled',
+                   vocabulary='plone.app.discussion.vocabularies.CaptchaVocabulary',)
 
-    show_commenter_image = schema.Bool(title=_(u"Show commenter image"),
-                                       description=_(u"help_show_commenter_image",
-                                                     default=u"If selected, an image of the "
-                                                              "user is shown next to the "
-                                                              "comment."),
+    show_commenter_image = schema.Bool(
+                               title=_(u"Show commenter image"),
+                               description=_(u"help_show_commenter_image",
+                                             default=u"If selected, an image "
+                                                       "of the user is shown "
+                                                       "next to the comment."),
                                        required=False,
                                        default=True)
 
