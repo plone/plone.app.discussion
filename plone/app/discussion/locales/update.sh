cf5f101538657f2308613b1c052d6595df6e1270
Thomas Desvenain <thomas.desvenain@gmail.com>
2011-03-17 15:30:31 +0000

    message extraction + french translations (previous commit was bad)
    svn path=/plone.app.discussion/trunk/; revision=48108

--- plone/app/discussion/locales/update.sh
+++ plone/app/discussion/locales/update.sh
@@ -1,3 +1,3 @@
 domain=plone.app.discussion
-i18ndude rebuild-pot --pot $domain.pot --create $domain ../
+i18ndude rebuild-pot --pot $domain.pot --create $domain --merge $domain-manual.pot ../
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
