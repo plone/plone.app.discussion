7b42c34493876779e6a5095e64b680d6e56d3b91
Vincent Fretin <vincent.fretin@gmail.com>
2011-04-10 20:37:37 +0000

    Nobody seems to maintain the LANGUAGES variable, remove the update-translations.sh script, use a simpler locales/update.sh script.
    svn path=/plone.app.discussion/trunk/; revision=48695

--- plone/app/discussion/locales/update.sh
+++ plone/app/discussion/locales/update.sh
@@ -1,3 +1,6 @@
 domain=plone.app.discussion
 i18ndude rebuild-pot --pot $domain.pot --create $domain --merge $domain-manual.pot ../
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
+
+i18ndude rebuild-pot --pot ../i18n/plone.pot --create plone --merge ../i18n/plone-manual.pot ../profiles
+i18ndude sync --pot ../i18n/plone.pot ../i18n/plone-*.po
