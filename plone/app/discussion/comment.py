702f7e1f1d7c2d50621f2f326c185ddb2034d174
Timo Stollenwerk <contact@timostollenwerk.net>
2012-06-17 12:09:25 +0200

    Make 'text/plain' the default mime type for comments and make sure the default type is set properly when creating a new comment.

--- plone/app/discussion/comment.py
+++ plone/app/discussion/comment.py
@@ -90,7 +90,7 @@ class Comment(CatalogAware, WorkflowAware, DynamicType, Traversable,
 
     title = u""
 
-    mime_type = None
+    mime_type = "text/plain"
     text = u""
 
     creator = None
@@ -205,6 +205,10 @@ def notify_content_object(obj, event):
     """Tell the content object when a comment is added
     """
     content_obj = aq_parent(aq_parent(obj))
+    # set the modified date and reindex the item accordingly
+    # so that 304s work correctly. This means that adding a comment
+    # effectively counts as modifying the content type.
+    content_obj.setModificationDate()
     content_obj.reindexObject(idxs=('total_comments',
                                     'last_comment_date',
                                     'commentators',
