6dffe930f9987b798ef4c46f54c90a43db49e2fd
Timo Stollenwerk <contact@timostollenwerk.net>
2012-11-16 12:09:40 +0100

    Pep8.

--- setup.py
+++ setup.py
@@ -22,22 +22,22 @@ install_requires = [
     'zope.lifecycleevent',
     'zope.site',
     'z3c.form>=2.3.3',
-    ]
+]
 
 setup(name='plone.app.discussion',
       version=version,
       description="Enhanced discussion support for Plone",
       long_description=open("README.rst").read() + "\n" +
-                       open("CHANGES.txt").read(),
+      open("CHANGES.txt").read(),
       classifiers=[
-        "Framework :: Plone",
-        "Framework :: Plone :: 3.3",
-        "Framework :: Plone :: 4.0",
-        "Framework :: Plone :: 4.1",
-        "Framework :: Plone :: 4.2",
-        "Framework :: Plone :: 4.3",
-        "Programming Language :: Python",
-        ],
+          "Framework :: Plone",
+          "Framework :: Plone :: 3.3",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Programming Language :: Python",
+      ],
       keywords='plone discussion',
       author='Timo Stollenwerk - Plone Foundation',
       author_email='plone-developers@lists.sourceforge.net',
