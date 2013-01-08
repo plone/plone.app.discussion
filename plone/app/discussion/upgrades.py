0e72ce12a6237cef1cae7fb0ac2cf99dfd86e584
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-01-08 10:31:16 +0100

    remove upgrade step from here. all upgrade step of plone goes to plone.app.upgrades

--- plone/app/discussion/upgrades.py
+++ plone/app/discussion/upgrades.py
@@ -7,9 +7,3 @@ def update_registry(context):
     registry = getUtility(IRegistry)
     registry.registerInterface(IDiscussionSettings)
 
-
-def add_anonymous_email_enabled_settings(context):
-    registry = getUtility(IRegistry)
-    key = 'plone.app.discussion.interfaces.IDiscussionSettings.'
-    key += 'anonymous_email_enabled'
-    registry[key] = False
