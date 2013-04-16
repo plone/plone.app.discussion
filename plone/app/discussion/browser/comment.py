72926ecb20f81b0a0d178062ad41a2809fc0db2d
Paul J Stevens <paul@nfg.nl>
2013-04-16 10:03:35 +0000

    add and hookup tisto's EditCommentForm

--- plone/app/discussion/browser/comment.py
+++ plone/app/discussion/browser/comment.py
@@ -1,7 +1,15 @@
 from Acquisition import aq_inner, aq_parent
+from AccessControl import getSecurityManager
 
+from zope.component import getMultiAdapter
+from Products.statusmessages.interfaces import IStatusMessage
 from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
+
+from plone.app.discussion import PloneAppDiscussionMessageFactory as _
+from comments import CommentForm
+from z3c.form import button
+from plone.z3cform.layout import wrap_form
 
 
 class View(BrowserView):
@@ -37,3 +45,65 @@ class View(BrowserView):
             url = "%s/view" % url
 
         self.request.response.redirect('%s#%s' % (url, context.id))
+
+
+class EditCommentForm(CommentForm):
+    """Form to edit an existing comment."""
+    ignoreContext = True
+    id = "edit-comment-form"
+    label = _(u'edit_comment_form_title', default=u'Edit comment')
+
+    def updateWidgets(self):
+        super(EditCommentForm, self).updateWidgets()
+        self.widgets['text'].value = self.context.text
+        # We have to rename the id, otherwise TinyMCE can't initialize
+        # because there are two textareas with the same id.
+        self.widgets['text'].id = 'overlay-comment-text'
+
+    def _redirect(self, target=''):
+        if not target:
+            portal_state = getMultiAdapter((self.context, self.request),
+                                       name=u'plone_portal_state')
+            target = portal_state.portal_url()
+        self.request.response.redirect(target)
+
+    @button.buttonAndHandler(_(u"edit_comment_form_button",
+                             default=u"Edit comment"), name='comment')
+    def handleComment(self, action):
+
+        # Validate form
+        data, errors = self.extractData()
+        if errors:
+            return
+
+        # Check permissions
+        can_reply = getSecurityManager().checkPermission(
+            'Modify portal content',
+            self.context)
+        mtool = getToolByName(self.context, 'portal_membership')
+        if mtool.isAnonymousUser() or not can_reply:
+            return
+
+        # Update text
+        self.context.text = data['text']
+
+        # Redirect to comment
+        IStatusMessage(self.request).add(_(u'comment_edit_notification',
+                                           default="Comment was edited"),
+                                         type='info')
+        return self._redirect(
+            target=self.action.replace(
+                "/edit-comment", "") + '#' + str(self.context.id))
+
+    @button.buttonAndHandler(_(u'cancel_form_button',
+                               default=u'Cancel'), name='cancel')
+    def handle_cancel(self, action):
+            IStatusMessage(self.request).add(
+                _(u'comment_edit_cancel_notification',
+                  default=u'Edit comment cancelled'),
+                type='info')
+            return self._redirect(target=self.context.absolute_url())
+
+EditComment = wrap_form(EditCommentForm)
+
+#EOF
