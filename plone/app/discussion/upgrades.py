770651cf72aabbf91aef53a44a7974aa9b84a944
Andrea Cecchi <andrea.cecchi85@gmail.com>
2014-03-03 16:15:12 +0100

    fixed upgrade step

--- plone/app/discussion/upgrades.py
+++ plone/app/discussion/upgrades.py
@@ -2,7 +2,13 @@ from zope.component import getUtility
 from plone.registry.interfaces import IRegistry
 from plone.app.discussion.interfaces import IDiscussionSettings
 
+default_profile = 'profile-plone.app.discussion:default'
+
 
 def update_registry(context):
     registry = getUtility(IRegistry)
     registry.registerInterface(IDiscussionSettings)
+
+
+def update_rolemap(context):
+    context.runImportStepFromProfile(default_profile, 'rolemap')
