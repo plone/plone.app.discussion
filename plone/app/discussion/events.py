d97f1d551e2f5faac54005f18a62079f276b7c9d
Alin Voinea <alin@eaudeweb.ro>
2014-04-17 18:38:44 +0300

    Add custom content rules string substitutions
    - Add content rules string substitutions for comment:
      - id
      - text
      - author email
      - author user name
      - author full name

--- plone/app/discussion/events.py
+++ plone/app/discussion/events.py
@@ -1,6 +1,7 @@
 """ Custom discussion events
 """
 from zope.interface import implements
+from plone.app.discussion.interfaces import IComment
 from plone.app.discussion.interfaces import IDiscussionEvent
 from plone.app.discussion.interfaces import ICommentAddedEvent
 from plone.app.discussion.interfaces import ICommentRemovedEvent
@@ -17,6 +18,17 @@ class DiscussionEvent(object):
         self.comment = comment
         for key, value in kwargs.items():
             setattr(self, key, value)
+
+        # Add comment on session to easily define content-rules dynamic strings
+        sdm = getattr(context, 'session_data_manager', None)
+        session = sdm.getSessionData(create=True) if sdm else None
+
+        if session:
+            sessionComment = dict(
+                (field, getattr(comment, field, None)) for field in IComment
+                if not field.startswith('_')
+            )
+            session.set('comment', sessionComment)
 
 class CommentAddedEvent(DiscussionEvent):
     """ Event to be triggered when a Comment is added
