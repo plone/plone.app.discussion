26a21a12d5d497c24bf5be27d37450b1ebbdf1f1
Gil Forcada <gforcada@gnome.org>
2016-04-07 18:30:30 +0200

    Reindex comments when they are modified

--- plone/app/discussion/subscribers.py
+++ plone/app/discussion/subscribers.py
@@ -3,7 +3,7 @@ from Products.CMFCore.utils import getToolByName
 
 
 def index_object(obj, event):
-    """Index the object when it is added to the conversation.
+    """Index the object when it is added/modified to the conversation.
     """
     catalog = getToolByName(obj, 'portal_catalog')
     return catalog.reindexObject(obj)
