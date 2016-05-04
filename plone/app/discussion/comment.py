89d542e8d2fa54388a5725afaf2cd19af6723b26
Maurits van Rees <maurits@vanrees.org>
2016-05-04 10:40:36 +0200

    Apply hotfix 20160419 (#94)
    Removed docstrings from some methods to avoid publishing them.

--- plone/app/discussion/comment.py
+++ plone/app/discussion/comment.py
@@ -127,13 +127,11 @@ class Comment(CatalogAware, WorkflowAware, DynamicType, Traversable,
         return self.comment_id and str(self.comment_id) or None
 
     def getId(self):
-        """The id of the comment, as a string.
-        """
+        # The id of the comment, as a string.
         return self.id
 
     def getText(self, targetMimetype=None):
-        """The body text of a comment.
-        """
+        # The body text of a comment.
         transforms = getToolByName(self, 'portal_transforms')
 
         if targetMimetype is None:
@@ -166,8 +164,7 @@ class Comment(CatalogAware, WorkflowAware, DynamicType, Traversable,
             return text
 
     def Title(self):
-        """The title of the comment.
-        """
+        # The title of the comment.
 
         if self.title:
             return self.title
@@ -192,14 +189,12 @@ class Comment(CatalogAware, WorkflowAware, DynamicType, Traversable,
         return title
 
     def Creator(self):
-        """The name of the person who wrote the comment.
-        """
+        # The name of the person who wrote the comment.
         return self.creator or self.author_name
 
     @security.protected(permissions.View)
     def Type(self):
-        """The Discussion Item content type.
-        """
+        # The Discussion Item content type.
         return self.fti_title
 
     # CMF's event handlers assume any IDynamicType has these :(
