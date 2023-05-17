3c5d329c3b9fe59b55b94509ea59050aa13a839e
Jens W. Klein <jk@kleinundpartner.at>
2023-05-17 15:41:38 +0200

    Turn this into a core addon, sync with CMFPlone, make installable and remove old files

--- setup.py
+++ setup.py
@@ -2,30 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "4.0.2.dev0"
-
-install_requires = [
-    "BTrees",
-    "Products.GenericSetup",
-    "Products.ZCatalog",
-    "Products.statusmessages",
-    "persistent",
-    "plone.api",
-    "plone.app.event",
-    "plone.dexterity",
-    "plone.registry",
-    "plone.resource",
-    "plone.uuid",
-    "zope.annotation",
-    "setuptools",
-    "plone.app.layout",
-    "plone.app.registry",
-    "plone.app.uuid",
-    "plone.base",
-    "plone.indexer",
-    "plone.z3cform",
-    "z3c.form>=2.3.3",
-]
+version = "5.0.0.dev0"
 
 setup(
     name="plone.app.discussion",
@@ -36,7 +13,7 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Framework :: Plone",
-        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: 6.1",
         "Framework :: Plone :: Core",
         "Framework :: Zope :: 5",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
@@ -57,7 +34,29 @@ setup(
     include_package_data=True,
     zip_safe=False,
     python_requires=">=3.8",
-    install_requires=install_requires,
+    install_requires=[
+        "Products.CMFPlone",
+        "BTrees",
+        "Products.GenericSetup",
+        "Products.ZCatalog",
+        "Products.statusmessages",
+        "persistent",
+        "plone.api",
+        "plone.app.event",
+        "plone.dexterity",
+        "plone.registry",
+        "plone.resource",
+        "plone.uuid",
+        "zope.annotation",
+        "setuptools",
+        "plone.app.layout",
+        "plone.app.registry",
+        "plone.app.uuid",
+        "plone.base",
+        "plone.indexer",
+        "plone.z3cform",
+        "z3c.form>=2.3.3",
+],
     extras_require={
         "test": [
             "plone.app.testing",
@@ -74,4 +73,8 @@ setup(
             "python-dateutil",
         ],
     },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
 )
