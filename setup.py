a052fa623cdffee7a65b994e385dcd1a6f139ef6
Maurits van Rees <maurits@vanrees.org>
2024-10-23 21:45:18 +0200

    Back to development: 5.0.0a3
    [ci skip]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (
 
 setup(
     name="plone.app.discussion",
-    version="5.0.0a2",
+    version="5.0.0a3.dev0",
     description="Enhanced discussion support for Plone",
     long_description=long_description,
     long_description_content_type="text/x-rst",
