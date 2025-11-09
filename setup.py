bcd8536eeb353d84ca16bb32325b5aab883785e3
Gil Forcada Codinachs <gil.gnome@gmail.com>
2025-11-09 09:51:04 +0100

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (
 
 setup(
     name="plone.app.discussion",
-    version="5.2.2.dev0",
+    version="6.0.0.dev0",
     description="Enhanced discussion support for Plone",
     long_description=long_description,
     long_description_content_type="text/x-rst",
