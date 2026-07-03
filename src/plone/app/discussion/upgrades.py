bd4c96fc856304dcab18dc798e361e0ecdfa5616
Mikel Larreategi <mlarreategi@codesyntax.com>
2026-07-03 07:23:36 +0200

    add upgrade step

--- src/plone/app/discussion/upgrades.py
+++ src/plone/app/discussion/upgrades.py
@@ -1,10 +1,12 @@
 from .interfaces import IDiscussionSettings
 from .setuphandlers import add_discussion_behavior_to_default_types
 from datetime import timezone
+from plone.base.utils import get_installer
 from plone.registry.interfaces import IRegistry
 from Products.CMFCore.utils import getToolByName
 from Products.ZCatalog.ProgressHandler import ZLogHandler
 from zope.component import getUtility
+
 
 import logging
 
@@ -119,3 +121,38 @@ def set_timezone_on_dates(context):
 def set_discussion_behavior(context):
     """Add the discussion behavior to all default types, if they exist."""
     add_discussion_behavior_to_default_types(context)
+
+
+def manage_comments_icon(context):
+    """if plone.app.discussion is installed, set the manage-comments action icon
+    expression from its default to the bootstrap based new-style icons
+    """
+
+    installer = get_installer(context)
+    if not installer.is_product_installed("plone.app.discussion"):
+        logger.info("plone.app.discussion is not installed. Nothing is done.")
+        return
+
+    portal_actions = getToolByName(context, "portal_actions")
+    user_actions = getattr(portal_actions, "user", None)
+    if user_actions is None:
+        logger.info("There are no user actions. Nothing is done.")
+        return
+
+    review_comments_action = getattr(user_actions, "review-comments", None)
+    if review_comments_action is None:
+        logger.info("There is no manage comments action. Nothing is done.")
+        return
+
+    # Check whether the action keeps its original state
+    if (
+        review_comments_action.icon_expr
+        != "string:${globals_view/navigationRootUrl}/discussionitem_icon.png"
+    ):
+        logger.info(
+            "Manage comments action icon was modified by the user. Nothing is done."
+        )
+        return
+
+    review_comments_action.icon_expr = "string:chat"
+    logger.info("Manage comments action icon modified")
